904194d288d5c56e68d19656fe9743affbec69aa
dinesh.gupta <dineshg009@gmail.com>
2023-12-29 18:14:09 +0530

    star program 2

--- src/main/java/org/example/BasicProgram/PrimeNumber.java
+++ src/main/java/org/example/BasicProgram/PrimeNumber.java
@@ -1,2 +1,20 @@
-package org.example.BasicProgram;public class PrimeNumber {
+package org.example.BasicProgram;
+
+public class PrimeNumber {
+
+    public static void primeNumber(int number){
+        int m= number/2;
+        if(number ==0 ||number == 1){
+            System.out.println(number +"is not a prime number");
+        }else {
+            for (int i=2;i<=m;i++){
+
+            }
+        }
+    }
+    public static void main(String[] args) {
+
+        primeNumber(13);
+
+    }
 }
