61dede5e3dc55bd07d9ab502a33d07a0f681880a
dinesh.gupta <dineshg009@gmail.com>
2023-12-29 18:26:08 +0530

    star program 4

---
+++ src/main/java/org/example/BasicProgram/starProgram4.java
@@ -0,0 +1,16 @@
+package org.example.BasicProgram;
+
+public class starProgram4 {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            for (int j = 5; j > i; j--) {
+                System.out.print("");
+            }
+            for (int k = 0; k <= i; k++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
